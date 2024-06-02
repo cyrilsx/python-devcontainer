240f102ad7bdb4883e65d77a7698d31eed6e1f56
cyril <scyril@collen.ch>
2024-06-02 19:54:59 +0000

    first test with devcontainer

---
+++ main.py
@@ -0,0 +1,4 @@
+from pymath import operation
+
+if __name__ == '__main__':
+    print(operation.Operation.sum(2, 3))
---
+++ pymath/operation.py
@@ -0,0 +1,10 @@
+class Operation:
+    
+    def __init__(self):
+        pass
+    
+    def sum(a, b):
+        return a + b
+    
+    
+    
